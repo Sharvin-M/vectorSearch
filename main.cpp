40b9d64f9a8e946f2c0f7fb137d59f621d41e1f2
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-06-30 19:27:54 -0700

    Can now read input from a file and print it out!

--- main.cpp
+++ main.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 
-typedef unsigned short int SHORT;
+// after the user inputs a filename, we are passing the user input by reference and opening it into the stream
 
-struct Cube
-{
-    SHORT w,l,h;
-public:
-    Cube()
-    {
-        w,l,h = 10;
-        SHORT* pW = &w;
-        SHORT* pL = &l;
-        SHORT* pH = &h;
+std::vector<std::string> read_input(const std::string &rFile) {
+  std::ifstream inputFile(rFile);
+
+  if (!inputFile.is_open()) {
+    std::cerr << "Error opening the file!" << std::endl;
+    return {};
+  }
+    std::string oneLine;
+    std::vector<std::string> multi;
+    for (int i = 0; i < 50; i++) {
+      getline(inputFile, oneLine);
+      multi.push_back(oneLine);
     }
-
-    int getVolume(Cube input) {
-        SHORT derefWidth = input.w;
-        SHORT derefLength = *pL;
-        SHORT derefHeight = *pH;
-        int volume = derefHeight * derefLength * derefWidth;
-        return volume;
-    }
-};
-
-int main()
-{
-    Cube firstCube;
-    std::cout << firstCube.getVolume() << std::endl;
-    printf("hello world\n");
-    return 0;
+    inputFile.close();
+  for (int i = 0; i < 10; i++) {
+    std::cout << multi.at(i) << std::endl;
+  }
+  return multi;
 }
 
 
-
-
-
-
+int main() {
+  std::string inFileName = "";
+  std::cin >> inFileName;
+  read_input(inFileName);
+  return 0;
+}
 
